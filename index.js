40c652b28837b804c828148a6532617adf4a4542
Wendy Wang <wendywangg928@gmail.com>
2023-07-06 17:39:50 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,4 +6,15 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*',(req,res)=>{      //'*' is a wild card symbol.Make another route below your homepage route with the path set to the wildcard operator. It is important that this route is at the bottom, otherwise it will override your other pages.
+    //Let's have our 404 page route legitimately return a status code 404 by calling .status(404) on the response. You can chain this call together with .send()
+    // add .status(404) b/t the res.send
+    res.status(404).send('<h1>404 Page</h1>')
+    
+
+
+})
+
+app.listen(process.env.PORT)
+
+
