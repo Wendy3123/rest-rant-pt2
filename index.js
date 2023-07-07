40070a203468a52af0ee087c1a80b1eef9c4f2ea
Wendy Wang <wendywangg928@gmail.com>
2023-07-07 16:17:27 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.get('/', (req, res) => {
 app.get('*',(req,res)=>{      //'*' is a wild card symbol.Make another route below your homepage route with the path set to the wildcard operator. It is important that this route is at the bottom, otherwise it will override your other pages.
     //Let's have our 404 page route legitimately return a status code 404 by calling .status(404) on the response. You can chain this call together with .send()
     // add .status(404) b/t the res.send
-    res.status(404).send('<h1>404 Page</h1>')
+    // res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
     
 
 
