5206c8a53b20f4feac92032e5fc1ccbb29939380
Wendy Wang <wendywangg928@gmail.com>
2023-07-07 16:14:49 -0400

    Adding home page view

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine','jsx')
+app.engine('jsx',require('express-react-views').createEngine())
+
 app.use('/places',require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello from Wendys world!')
+    // res.send('Hello from Wendys world!')
+    res.render('home')
 })
 
 app.get('*',(req,res)=>{      //'*' is a wild card symbol.Make another route below your homepage route with the path set to the wildcard operator. It is important that this route is at the bottom, otherwise it will override your other pages.
